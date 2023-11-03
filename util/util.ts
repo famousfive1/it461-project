5b3465b0f77df8cce67088af88be922122c912ac
Tejaswi Hegde <tejaswihegde1@gmail.com>
2023-11-03 22:22:37 +0530

    Change util script

--- util/util.ts
+++ util/util.ts
@@ -1,7 +1,9 @@
 import shell from "shelljs";
 
 const n_nodes = 2;
-const op = process.argv[2];
+let op = process.argv[2];
+if(process.argv[3]) op += ' ' + process.argv[3];
+if(process.argv[4]) op += ' ' + process.argv[4];
 
 for(let i = 1; i <= n_nodes; i++) {
     shell.exec(`prisma ${op} --schema prisma/schema${i}.prisma`);
