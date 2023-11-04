703c052d4925e1814f5e119958430400dec539d2
satyam-kalyane <satyamkalyane53@gmail.com>
2023-11-04 20:34:21 +0530

    game queries

---
+++ scripts/queries/game_q3.ts
@@ -0,0 +1,35 @@
+
+import { PrismaClient as PClient1 } from "../../prisma/generated/client1";
+
+const prisma1 = new PClient1();
+
+
+async function main() {
+  const game_price = await prisma1.game.findMany({
+    where: {
+        price: {
+        
+        }
+    },
+    select: {
+      id: true,
+      name: true,
+      price: true,
+    },
+  });
+
+  
+  console.log(game_price);
+}
+
+main()
+  .then(async () => {
+    await prisma1.$disconnect();
+
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma1.$disconnect();
+
+    process.exit(1);
+  });
