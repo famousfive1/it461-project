0fd9f6aabeac8ccaee2222499a959e1ec7e42407
Tejaswi Hegde <tejaswihegde1@gmail.com>
2023-11-04 17:40:10 +0530

    Code refactor

--- scripts/add/user.ts
+++ scripts/add/user.ts
@@ -5,37 +5,21 @@ import { PrismaClient as PClient4 } from "../../prisma/generated/client4";
 
 import { alpha as data } from "../../util/random";
 
-const prisma1 = new PClient1();
-const prisma2 = new PClient2();
-const prisma3 = new PClient3();
-const prisma4 = new PClient4();
+const prisma = [new PClient1(), new PClient2(), new PClient3(), new PClient4()];
 
 async function main() {
-    await Promise.all([
-        prisma1.user.createMany({ data: data.slice(0, 25)   }),
-        prisma2.user.createMany({ data: data.slice(25, 50)  }),
-        prisma3.user.createMany({ data: data.slice(50, 75)  }),
-        prisma4.user.createMany({ data: data.slice(75, 100) })
-    ]);
+    await Promise.all(prisma.map(
+        (c, i) => c.user.createMany({ data: data.slice(25*i, 25*i + 25) })
+    ));
     console.log("Added");
 };
 
 main()
     .then(async () => {
-        await Promise.all([
-            prisma1.$disconnect(),
-            prisma2.$disconnect(),
-            prisma3.$disconnect(),
-            prisma4.$disconnect(),
-        ]);
+        await Promise.all(prisma.map( c => c.$disconnect()));
     })
     .catch(async (e) => {
         console.error(e)
-        await Promise.all([
-            prisma1.$disconnect(),
-            prisma2.$disconnect(),
-            prisma3.$disconnect(),
-            prisma4.$disconnect(),
-        ]);
+        await Promise.all(prisma.map( c => c.$disconnect()));
         process.exit(1)
     });
