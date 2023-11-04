f3bbd88b8fdadcd9145ffa935ad898ee8d279b2d
Pratham Patel <pratham101112@gmail.com>
2023-11-04 23:11:21 +0530

    fix add for windows

--- scripts/add.ts
+++ scripts/add.ts
@@ -1,11 +1,19 @@
-import { spawnSync } from "child_process";
+import { execSync, exec } from "child_process";
 import { join } from "path";
 
-spawnSync('npx', ['ts-node', join(__dirname, "add", "user.ts")], { stdio: 'inherit' });
+// spawnSync('npx', ['ts-node', join(__dirname, "add", "user.ts")], { stdio: 'inherit' });
+exec(`npx ts-node ${join(__dirname, "add", "user.ts")}`);
+// execSync(`npx ts-node ${join(__dirname, "add", "user.ts")}`, { stdio: 'inherit' });
 console.log("Added Users");
-spawnSync('npx', ['ts-node', join(__dirname, "add", "game.ts")], { stdio: 'inherit' });
+// spawnSync('npx', ['ts-node', join(__dirname, "add", "game.ts")], { stdio: 'inherit' });
+exec(`npx ts-node ${join(__dirname, "add", "game.ts")}`);
+// execSync(`npx ts-node ${join(__dirname, "add", "game.ts")}`, { stdio: 'inherit' });
 console.log("Added Games");
-spawnSync('npx', ['ts-node', join(__dirname, "add", "review.ts")], { stdio: 'inherit' });
+// spawnSync('npx', ['ts-node', join(__dirname, "add", "review.ts")], { stdio: 'inherit' });
+exec(`npx ts-node ${join(__dirname, "add", "review.ts")}`);
+// execSync(`npx ts-node ${join(__dirname, "add", "review.ts")}`, { stdio: 'inherit' });
 console.log("Added Reviews");
-spawnSync('npx', ['ts-node', join(__dirname, "add", "trans.ts")], { stdio: 'inherit' });
+// spawnSync('npx', ['ts-node', join(__dirname, "add", "trans.ts")], { stdio: 'inherit' });
+exec(`npx ts-node ${join(__dirname, "add", "trans.ts")}`);
+// execSync(`npx ts-node ${join(__dirname, "add", "trans.ts")}`, { stdio: 'inherit' });
 console.log("Added Transactions");
