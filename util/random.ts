78589c1c2fe450eac2f41dfed3c52eb97660a973
Tejaswi Hegde <tejaswihegde1@gmail.com>
2023-11-04 20:02:34 +0530

    Schema changes

--- util/random.ts
+++ util/random.ts
@@ -1,4 +1,13 @@
 import { faker } from '@faker-js/faker';
+import { join } from 'path';
+import fs from 'fs';
+import { parse } from 'csv/sync';
+
+const content = fs.readFileSync(join(__dirname, 'final_data.csv'));
+const records = parse(content, {
+    columns: true,
+    skip_empty_lines: true
+});
 
 function generateRandomUserData(i: number): {
     id: number;
@@ -23,7 +32,7 @@ function generateRandomTransactions(i: number): {
     const game_id = faker.number.int(99);
     const user_id = faker.number.int(99);
     const amount = faker.number.float({ min: 0, max: 100, precision: 0.01 });
-    const status = "Success";
+    const status = "";
     return { id, game_id, user_id, amount, status };
 }
 
@@ -42,11 +51,27 @@ function generateRandomReviews(i: number): {
     return { id, game_id, user_id, review, rating };
 }
 
+function generateGames(i: number): {
+    id: number;
+    name: string;
+    release_date: string;
+    developer: string;
+    genre: string;
+    price: number
+} {
+    const id = i;
+    const name = "";
+    const release_date = "";
+    const developer = "";
+    const genre = "";
+    const price = 0;
+    return { id, name, release_date, developer, genre, price };
+}
+
 const count = 5;
 const iter = Array.apply(null, Array(count));
 export const user = iter.map((_, i) => generateRandomUserData(i));
 export const trans = iter.map((_, i) => generateRandomTransactions(i));
 export const reviews = iter.map((_, i) => generateRandomReviews(i));
-
-console.log(user, trans, reviews);
+export const games = iter.map((_, i) => generateGames(i));
 
